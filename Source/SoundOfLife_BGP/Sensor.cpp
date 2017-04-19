4aa197b8846368829d0643cea2e50cb6684c4edd
MalcolmYllenius <malcolm@live.se>
2017-04-19 15:05:44 +0200

    Sensors

---
+++ Source/SoundOfLife_BGP/Sensor.cpp
@@ -0,0 +1,104 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SoundOfLife_BGP.h"
+#include "Sensor.h"
+#include "VictimLocation.h"
+
+
+// Sets default values
+ASensor::ASensor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	USphereComponent* sphere = CreateDefaultSubobject<USphereComponent>(TEXT("DetectionSphere"));
+	RootComponent = sphere;
+	sphere->InitSphereRadius(500.0f);
+	detectionSphere = sphere;
+
+	UStaticMeshComponent* staticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	staticMesh->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> staticMeshAsset(TEXT("StaticMesh'/Game/Sensor_LP.Sensor_LP'"));
+	if (staticMeshAsset.Succeeded())
+	{
+		staticMesh->SetStaticMesh(staticMeshAsset.Object);
+	}
+
+	audioParent = CreateDefaultSubobject<USceneComponent>(TEXT("AudioParent"));
+	audioParent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ASensor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> overlappingActors;
+	detectionSphere->GetOverlappingActors(overlappingActors, AActor::StaticClass());
+	for (int i = 0; i < overlappingActors.Num(); i++)
+	{
+		if (overlappingActors[i]->IsA(AVictimLocation::StaticClass()))
+		{
+			AVictimLocation* victimLocation = (AVictimLocation*)overlappingActors[i];
+			bool isNew = true;
+			for (int j = 0; j < victimsWithinProximity.Num(); j++)
+			{
+				if (victimLocation == victimsWithinProximity[j])
+				{
+					isNew = false;
+					break;
+				}
+			}
+			if (isNew)
+				victimsWithinProximity.Add(victimLocation);
+		}
+	}
+
+	audioList = GetAudio();
+	PlayAudio(1);
+}
+
+// Called every frame
+void ASensor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+TArray<TPair<float, USoundCue*>> ASensor::GetAudio()
+{
+	TArray<TPair<float, USoundCue*>> audioList;
+	for (int i = 0; i < victimsWithinProximity.Num(); i++)
+	{
+		TPair<float, USoundCue*> pair;
+		pair.Key = (detectionSphere->GetComponentLocation() - victimsWithinProximity[i]->GetActorLocation()).Size();
+		pair.Value = ((AVictimLocation*)victimsWithinProximity[i])->GetAudio();
+		audioList.Add(pair);
+	}
+	TPair<float, USoundCue*> pair;
+	pair.Key = .0f;
+	pair.Value = backgroundNoise;
+	audioList.Add(pair);
+	return audioList;
+}
+
+void ASensor::PlayAudio(float volume)
+{
+	for (int i = 0; i < audioList.Num(); i++)
+	{
+		FString string = FString::Printf(TEXT("AudioSource %i"), i);
+		UAudioComponent* audioComponent = NewObject<UAudioComponent>(this, FName(*string));
+		audioComponent->SetSound(audioList[i].Value);
+		float amplitude = volume * (1.0f - (audioList[i].Key / detectionSphere->GetUnscaledSphereRadius()));
+		audioComponent->FadeIn(0, amplitude, 0);
+		audioComponent->AttachTo(audioParent);
+	}
+}
+
+void ASensor::StopAudio()
+{
+	while (audioParent->GetNumChildrenComponents() > 0)
+	{
+		UAudioComponent* audioComponent = (UAudioComponent*)audioParent->GetChildComponent(0);
+		audioComponent->Stop();
+		audioComponent->DestroyComponent();
+	}
+}
---
+++ Source/SoundOfLife_BGP/Sensor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Sensor.generated.h"
+
+UCLASS()
+class SOUNDOFLIFE_BGP_API ASensor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASensor();
+	UPROPERTY(EditAnywhere)
+		USphereComponent* detectionSphere;
+	UPROPERTY(VisibleAnywhere)
+		TArray<AActor*> victimsWithinProximity;
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* mesh;
+	UPROPERTY(EditAnywhere)
+		USoundCue* backgroundNoise;
+	USceneComponent* audioParent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	TArray<TPair<float, USoundCue*>> audioList;
+	TArray<TPair<float, USoundCue*>> GetAudio();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable)
+		void PlayAudio(float volume);
+	UFUNCTION(BlueprintCallable)
+		void StopAudio();
+};
